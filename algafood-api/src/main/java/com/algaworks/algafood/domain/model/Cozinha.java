e06a137373644cf1ec71a38876898d54476b3327
Marcelo Pituba <mpituba@yahoo.com>
2022-12-07 10:34:54 -0300

    3.7. Importando dados de teste com import.sql

--- algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -2,12 +2,11 @@ package com.algaworks.algafood.domain.model;
 
 import java.util.Objects;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
+
 
 @Entity
 public class Cozinha {
